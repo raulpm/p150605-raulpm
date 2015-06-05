8af620f9148340c2e2e6c43bda7921a35ae1367b
raulpm <raulpastormartos@gmail.com>
2015-06-05 12:37:11 +0200

    Tras documentar Javadoc Raúl Pastor
    Tras documentar Javadoc Raúl Pastor

--- src/p20150605/AudioSpot.java
+++ src/p20150605/AudioSpot.java
@@ -2,6 +2,16 @@ package p20150605;
 
 import java.io.File;
 
+
+/**
+ * Gestiona cuñas de anuncios publicitarios en radio.
+ * Cada objeto que se cree en esta clase será una cuña diferente
+ * 
+ * @author	raulpm
+ * @version 1.0.1
+ * @see	P20150605
+ * 
+ */
 public class AudioSpot
 {
     private File archivo;       // manejador para el archivo que contiene el audio (.mp3)
@@ -9,8 +19,19 @@ public class AudioSpot
     private String producto;    // nombre del producto anunciado
     private String anunciante;  // nombre de la empresa anunciante
     
+    /**
+     * 
+     * Contiene una descripción del error relaccionado con el codigo negativo devuelto
+     * 
+     */
     public String lasterrormsg;
     
+    
+    /**
+     * Constructor de la clase AudioSpot
+     * @param : Sin parametros
+     * 
+     */
     public AudioSpot ()
     {
         this.archivo = null;
@@ -20,12 +41,25 @@ public class AudioSpot
         this.lasterrormsg = "";
     }
     
+    /**
+     * 
+     * Almacena metadatos sobre el <b>producto</b> y el <b>anunciante</b>
+     * @param producto : establece el nombre del producto
+     * @param anunciante : establece el nombre del anunciante
+     * 
+     */
     public void setMetaDatos (String producto, String anunciante)
     {
         this.producto = producto;
         this.anunciante = anunciante;
     }
     
+    /**
+     * 
+     * Establece la <b>duración</b> de la cuña
+     * @param duracion : establece la duracion de la cuña
+     * @exception: No se permiten cuñas de mas de 120
+     */
     public void setDuracion(int duracion) throws IllegalArgumentException
     {
         if (duracion<0)
@@ -35,12 +69,32 @@ public class AudioSpot
         this.duracion = duracion;
     }
     
+    /**
+     * 
+     * Vincula la clase con el archivo de audio que contiene la cuña
+     * Se asegura de que el archivo existe
+     * @param nombre_archivo : Establece el nombre del archivo y su ruta
+     * @return si el archivo existe devuelve 'true'
+     */
     public Boolean setArchivo(String nombre_archivo)
     {
         this.archivo = new File(nombre_archivo);
         return this.archivo.exists();
     }
     
+    
+    /**
+     * Programa la cuña dentro de una secuencia de audio clips
+     * Comprueba que no falta ningun parametro y que el archivo existe
+     * @param : cola_reproduccion : secuencia de audio
+     * @return : -1 : si no hay establecida duración, </br> 
+     * -2 : si no hay establecido nombre,</br> 
+     * -3 : Si no hay establecido anunciante,</br> 
+     * -4 si no hay archivo de audio</br>
+     * -5 si hay problemas en la introducción del anuncio en la cola de reproducción
+     * 
+     * 
+     */
     public int ProgramaEnCola(Object cola_reproduccion)
     {
         // comprobamos previamente que no falte nada
@@ -84,6 +138,18 @@ public class AudioSpot
         else
             return resultado;
     }
+    
+    /**
+     * 
+     * Crea un fichero .daf(Daw Audio Format) con la exportación de la cuña completa
+     * @param : objeto_daw : cuña completa a exportar
+     * @return : -1 : si no hay establecida duración, </br> 
+     * -2 : si no hay establecido nombre,</br> 
+     * -3 : Si no hay establecido anunciante,</br> 
+     * -4 si no hay archivo de audio</br>
+     * -5 si hay problemas en la introducción del anuncio en la cola de reproducción</br>
+     * 0 si se exporta correctamente
+     */
     
     public int ExportaAFormatoDAW(Object objeto_daw)
     {
--- src/p20150605/P20150605.java
+++ src/p20150605/P20150605.java
@@ -1,5 +1,11 @@
 package p20150605;
 
+/**
+ * @author raulpm
+ * @version 1.0.1
+ * @see AudioSpot
+ * 
+ */
 public class P20150605 {
 
     public static void main(String[] args)
