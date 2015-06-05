64f4dd734f63f2b5cc88c7bd2d86a2891a9a3b45
raulpm <raulpastormartos@gmail.com>
2015-06-05 11:53:21 +0200

    Primera versión de P20150605 Raúl Pastor
    Los archivos son subidos

---
+++ src/p20150605/AudioSpot.java
@@ -0,0 +1,132 @@
+package p20150605;
+
+import java.io.File;
+
+public class AudioSpot
+{
+    private File archivo;       // manejador para el archivo que contiene el audio (.mp3)
+    private int duracion;       // duración del audio, en segundos
+    private String producto;    // nombre del producto anunciado
+    private String anunciante;  // nombre de la empresa anunciante
+    
+    public String lasterrormsg;
+    
+    public AudioSpot ()
+    {
+        this.archivo = null;
+        this.duracion = 0;
+        this.producto = "";
+        this.anunciante = "";
+        this.lasterrormsg = "";
+    }
+    
+    public void setMetaDatos (String producto, String anunciante)
+    {
+        this.producto = producto;
+        this.anunciante = anunciante;
+    }
+    
+    public void setDuracion(int duracion) throws IllegalArgumentException
+    {
+        if (duracion<0)
+            duracion=0;
+        else if (duracion>120)
+            throw new IllegalArgumentException ("Duración demasiado larga");
+        this.duracion = duracion;
+    }
+    
+    public Boolean setArchivo(String nombre_archivo)
+    {
+        this.archivo = new File(nombre_archivo);
+        return this.archivo.exists();
+    }
+    
+    public int ProgramaEnCola(Object cola_reproduccion)
+    {
+        // comprobamos previamente que no falte nada
+        int resultado = 0;
+        if (this.duracion==0)
+        {
+            lasterrormsg = "No se ha establecido duración alguna.";
+            resultado = -1;
+        }
+        else if (this.producto.equals(""))
+        {
+            lasterrormsg = "No se ha indicado el nombre del producto anunciado.";
+            resultado = -2;
+        }
+        else if (this.anunciante.equals(""))
+        {
+            lasterrormsg = "No se ha indicado el nombre de la empresa anunciante.";
+            resultado = -3;
+        }
+        else if (this.archivo==null)
+        {
+            lasterrormsg = "No se ha establecido el archivo de audio.";
+            resultado = -4;
+        }
+        
+        // si todo va bien, programamos
+        if (resultado==0)
+        {
+            try
+            {
+                // falta por implementar la programación en la cola_reproducción
+                lasterrormsg = "";
+                return this.duracion;
+            }
+            catch (Exception ex)
+            {
+                lasterrormsg = ex.getMessage();
+                return -5;
+            }     
+        }
+        else
+            return resultado;
+    }
+    
+    public int ExportaAFormatoDAW(Object objeto_daw)
+    {
+        // comprobamos previamente que no falte nada
+        int resultado = 0;
+        if (this.duracion==0)
+        {
+            lasterrormsg = "No se ha establecido duración alguna.";
+            resultado = -1;
+        }
+        else if (this.producto.equals(""))
+        {
+            lasterrormsg = "No se ha indicado el nombre del producto anunciado.";
+            resultado = -2;
+        }
+        else if (this.anunciante.equals(""))
+        {
+            lasterrormsg = "No se ha indicado el nombre de la empresa anunciante.";
+            resultado = -3;
+        }
+        else if (this.archivo==null)
+        {
+            lasterrormsg = "No se ha establecido el archivo de audio.";
+            resultado = -4;
+        }
+        
+        // si todo va bien, exportamos
+        if (resultado==0)
+        {
+            try
+            {
+                // falta por implementar la exportación del audio
+                lasterrormsg = "";
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                lasterrormsg = ex.getMessage();
+                return -5;
+            }
+        }
+        else
+            return resultado;
+    }
+    
+}
---
+++ src/p20150605/P20150605.java
@@ -0,0 +1,10 @@
+package p20150605;
+
+public class P20150605 {
+
+    public static void main(String[] args)
+    {
+        //
+    }
+    
+}
